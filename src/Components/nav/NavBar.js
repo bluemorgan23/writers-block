a873a6442389bafcd812474eba9ff37584e1bfd0
Chris Morgan <bluemorgan2323@gmail.com>
2019-04-29 12:07:02 -0500

    Put flex on the navbar container, and put equal spacing around the nav links. Imported navbar.css into NavBar.js

--- src/Components/nav/NavBar.js
+++ src/Components/nav/NavBar.js
@@ -1,12 +1,15 @@
 import React, {Component} from "react"
-import {Nav, NavLink} from "reactstrap"
 import {Link} from "react-router-dom"
+
+import {Nav} from "reactstrap"
+
+import "./navbar.css"
 
 export default class NavBar extends Component {
     
     render() {
         return (
-            <Nav>
+            <Nav className="navBar">
                 <Link className="nav-link" to="/new-entry">New Entry</Link>
                 <Link className="nav-link" to="/results">Results</Link>
                 <Link className="nav-link" to="/stats">Stats</Link>
