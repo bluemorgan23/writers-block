1024e6fbe5480253ca89d990f55a95a0a0ae5f3a
Chris Morgan <bluemorgan2323@gmail.com>
2019-04-29 11:37:25 -0500

    First commit, react-router-dom, bootstrap, and reactstrap added to create-react-app boilerplate depenedencies. Created an api directore with database.json and database.json.example. Database.json is in .gitignore. Set up the directories to my liking with components and modules directories.

---
+++ src/Components/ApplicationViews.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ApplicationViews extends Component {
+    render(){
+        return (
+            <div>
+                Hello World
+            </div>
+        )
+    }
+}
+
+export default ApplicationViews
