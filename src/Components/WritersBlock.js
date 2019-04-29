dbf01346323c4fcb3b5f05b768e1281ce9c25974
Chris Morgan <bluemorgan2323@gmail.com>
2019-04-29 11:50:03 -0500

    Created WritersBlock.js which will be responsible for rendering both the NavBar and ApplicationViews components. Imported BrowserRouter into index.js and wrapped the rendering with Router in order to use dynamic routing in my navbar.

---
+++ src/Components/WritersBlock.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+import ApplicationViews from "./ApplicationViews";
+
+export default class WritersBlock extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <ApplicationViews />
+            </React.Fragment>
+        )
+    }
+}
